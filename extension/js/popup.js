8dc7b37c96c8f97096b65c28bfa11fd73b022b2f
S Stewart <tilda@users.noreply.github.com>
2020-06-22 01:44:03 -0500

    add comments im bored

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -2,15 +2,15 @@ var dot = document.getElementById('dot')
 var status = document.getElementById('status')
 var connection = document.getElementById('connection')
 
-if (globalThis.ws.readyState === 1) {
+if (globalThis.ws.readyState === 1) { // Connected
     dot.classList.add('colors-online')
     status.innerHTML = 'Connected'
     connection.innerHTML = 'Disconnect'
-} else if (globalThis.ws.readyState === 0) {
+} else if (globalThis.ws.readyState === 0) { // Connecting
     dot.classList.add('colors-connecting')
     status.innerHTML = 'Connecting...'
     connection.setAttribute(disabled, true)
-} else if (globalThis.ws.readyState === 3) {
+} else if (globalThis.ws.readyState === 3) { // Disconnected
     dot.classList.add('colors-offline')
     status.innerHTML = 'Failed to connect'
     connection.innerHTML = 'Connect'
