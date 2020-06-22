34c916abce956cff1fea62598717d9f85d178a51
S Stewart <tilda@users.noreply.github.com>
2020-06-22 01:40:13 -0500

    put in more of that ui stuff

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,3 +1,17 @@
 var dot = document.getElementById('dot')
 var status = document.getElementById('status')
 var connection = document.getElementById('connection')
+
+if (globalThis.ws.readyState === 1) {
+    dot.classList.add('colors-online')
+    status.innerHTML = 'Connected'
+    connection.innerHTML = 'Disconnect'
+} else if (globalThis.ws.readyState === 0) {
+    dot.classList.add('colors-connecting')
+    status.innerHTML = 'Connecting...'
+    connection.setAttribute(disabled, true)
+} else if (globalThis.ws.readyState === 3) {
+    dot.classList.add('colors-offline')
+    status.innerHTML = 'Failed to connect'
+    connection.innerHTML = 'Connect'
+}
